cea0ae01d2f817f80f71f6668abc5e1e7ae07f50
Sidney Júnior <junior.lopes@luizalabs.com>
2022-12-19 11:12:56 -0300

    group by no rank

--- relatorio/relatorio.py
+++ relatorio/relatorio.py
@@ -26,9 +26,18 @@ def fechar_conexao(conexao):
     conexao.close()
 
 total_vendas = "SELECT COUNT(nome) as total_produtos, ROUND(SUM(quantidade),2) as total_quantidade, ROUND(SUM(preco), 2) as total_preco from VENDAS;"
-rank_vendas = "SELECT * FROM VENDAS ORDER BY quantidade DESC;" 
+rank_vendas ='''SELECT nome, preco, sum(quantidade) as quantidade, round(sum(quantidade) * preco, 2) as total 
+                FROM VENDAS 
+                GROUP BY nome, preco 
+                ORDER BY quantidade DESC
+'''
 consulta_vendas = "SELECT * FROM VENDAS;"
-rank_10_vendas = "SELECT * FROM VENDAS ORDER BY quantidade DESC LIMIT 10;"
+rank_10_vendas = '''SELECT nome, preco, sum(quantidade) as quantidade, round(sum(quantidade) * preco, 2) as total 
+                    FROM VENDAS 
+                    GROUP BY nome, preco 
+                    ORDER BY quantidade DESC
+                    LIMIT 10;
+'''
 relatorio_vendas = "SELECT * FROM VENDAS"
 relatorio_vendas_agrupado = "SELECT nome, preco, sum(quantidade) as quantidade, round(sum(quantidade) * preco, 2) as total FROM VENDAS GROUP BY nome, preco;"
 limpar_vendas = "DELETE FROM VENDAS"
